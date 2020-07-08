217d6eb70d1d27fc994c5f0b6e07e995151f878c
Ramesh Aroor <jobseek77@gmail.com>
2020-07-08 16:41:40 -0400

    # WARNING: head commit changed in the meantime
    cimmit

--- testng-demo-master/src/test/java/com/testngdemo/automation/utils/SeleniumGridConfig.java
+++
@@ -1,6 +0,0 @@
-package com.testngdemo.automation.utils;
-
-public class SeleniumGridConfig {
-    public final static String SELENIUM_GRID_HUB_IP = "localhost";
-    public final static String SELENIUM_GRID_HUB_PORT ="4444";
-}
