2f1d6f196c4011ee64571d9b60a2abcb5c8e2fbd
Ramesh Aroor <jobseek77@gmail.com>
2020-07-08 16:39:25 -0400

    intial draft1

---
+++ testng-demo-master/src/main/java/com/testngdemo/automation/App.java
@@ -0,0 +1,13 @@
+package com.testngdemo.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
